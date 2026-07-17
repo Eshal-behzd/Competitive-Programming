0e34cc6332b2292f348287b5b069adebb17cd4ad
Eshal-behzd <behzadeshal5@gmail.com>
2026-07-17 09:43:25 -0700

    Add For_Loop, If_Statement, and While_loop classes to demonstrate basic control flow in Java

---
+++ If_Statement.java
@@ -0,0 +1,12 @@
+public class If_Statement {
+    public static void main(String[]args){
+        int num = 10;
+        if (num == 10){
+            System.out.println("The number is 10");
+        
+        }
+        else {
+            System.out.println("The number is not 10");
+        }
+    }
+}
