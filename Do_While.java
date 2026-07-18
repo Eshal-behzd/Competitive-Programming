9eb1e4548d94133d50a3440a5063535f9c1cb620
Eshal-behzd <behzadeshal5@gmail.com>
2026-07-18 11:00:36 -0700

    Add Do_While class to demonstrate the do-while loop in Java

---
+++ Do_While.java
@@ -0,0 +1,10 @@
+public class Do_While {
+    public static void main(String[] args){
+        int i = 1;
+        do {
+            System.out.println("Hello World");
+        }
+        while (i != 1);
+        i++;
+    }
+}
