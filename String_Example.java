6575988b6e5b19b97f77c84daf7d0367477b229a
Eshal-behzd <behzadeshal5@gmail.com>
2026-07-24 10:16:02 -0700

    Add String_Example class to demonstrate a simple greeting in Java

---
+++ String_Example.java
@@ -0,0 +1,10 @@
+public class String_Example {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.print(message);
+    }
+    static String greet() {
+        String greeting = "Hi! How are You?";
+        return greeting;
+    }
+}
