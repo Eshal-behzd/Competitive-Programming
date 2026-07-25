a5b5be53984968bfc89dacd2f5e0f668de679420
Eshal-behzd <behzadeshal5@gmail.com>
2026-07-25 09:55:22 -0700

    Add Arguments class to demonstrate a simple sum calculation in Java

---
+++ Arguments.java
@@ -0,0 +1,12 @@
+public class Arguments {
+    public static void main(String[] args){
+        int ans = sum(30, 20);
+        System.out.print(ans);
+
+    }
+    static int sum(int a, int b) {
+        int sum1 = a + b;
+        return sum1;
+
+    }
+}
