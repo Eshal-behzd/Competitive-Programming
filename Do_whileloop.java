9c1307794a90298e21a8bc9b7cb4ed469d8b2d41
Eshal-behzd <behzadeshal5@gmail.com>
2026-07-31 13:58:44 -0700

    Add Do_whileloop class to demonstrate do-while loop functionality in Java

--- Do_whileloop.java
+++ Do_whileloop.java
@@ -1,10 +1,10 @@
-public class While_loop {
+public class Do_whileloop {
     public static void main(String[] args) {
-        int i = 0;
-        while (i != 5){
+        int i = 1;
+
+        do {
             System.out.println(i);
             i++;
-        }
+        } while (i <= 5);
     }
 }
-
